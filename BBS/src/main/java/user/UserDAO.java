d1923b7427c869648531ef81ec2d4bd7ddd572c5
Kminseung <soccer6504@gmail.com>
2021-09-15 15:11:31 +0900

    Add login function, Construct user database

---
+++ BBS/src/main/java/user/UserDAO.java
@@ -0,0 +1,51 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	// Object that accesses the database
+	private Connection conn;
+	// To prevent SQL Injection
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/BBS";
+			String dbID = "root";
+			String dbPassword = "7777";
+			Class.forName("com.mysql.jdbc.Driver");	
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		
+		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1;	// Success to login
+				} else {
+					return 0;	// Password mismatch
+				}
+			}
+			return -1;		// No userID
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return  -2;		// Database Error
+	}
+	
+}
